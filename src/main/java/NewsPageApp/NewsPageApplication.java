48c8d4fed0c78611ac8e64660efec7c2d3458b82
Mikhail <mcmishabest@gmail.com>
2021-11-04 16:18:26 +0600

    Initial commit

---
+++ src/main/java/NewsPageApp/NewsPageApplication.java
@@ -0,0 +1,13 @@
+package NewsPageApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsPageApplication.class, args);
+	}
+
+}
