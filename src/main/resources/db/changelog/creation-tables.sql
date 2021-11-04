0ddd9ac7d474ce43bbdd315584b920d06f9dac69
Mikhail <mcmishabest@gmail.com>
2021-11-04 16:54:34 +0600

    project configuration, adding migrations

---
+++ src/main/resources/db/changelog/creation-tables.sql
@@ -0,0 +1,20 @@
+--liquibase formatted sql
+--changeset M:create-all-tables
+
+CREATE TABLE IF NOT EXISTS news_type
+(
+    id         SERIAL  NOT NULL,
+    name       TEXT    NOT NULL,
+    color      TEXT    NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS news
+(
+    id          SERIAL  NOT NULL,
+    short_desc  TEXT    NOT NULL,
+    full_desc   TEXT    NOT NULL,
+    type_id     INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (type_id) REFERENCES news_type (id)
+);
