5d55321f817e7ce28796c719dc5b43f5bfd4ce67
Mikhail <mcmishabest@gmail.com>
2021-11-15 10:33:49 +0600

    fix model - news

--- src/main/java/NewsPageApp/models/News.java
+++ src/main/java/NewsPageApp/models/News.java
@@ -1,6 +1,8 @@
 package NewsPageApp.models;
 
 import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 
@@ -12,9 +14,14 @@ public class News {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String name;
+
     private String shortDesc;
+
     private String fullDesc;
-    @OneToOne
+
+    @OneToOne(cascade = CascadeType.MERGE)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private NewsType type;
 }
